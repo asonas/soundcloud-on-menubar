2c03b9eaffee6dae74acff3170a830d3aef673f9
Yuya Fujiwara <asonas@cookpad.com>
2020-09-17 17:29:22 +0900

    Initial Commit

---
+++ soundcloud-on-menubar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  soundcloud-on-menubar
+//
+//  Created by asonas on 2020/09/17.
+//  Copyright © 2020 asonas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
